8c8df80881b572806b0db35e6c8f9841f3467128
mayonezny <borischubuhchiev@mail.ru>
2026-04-07 01:01:45 +0300

    фикс очередной

--- src/features/authorization/api/auth.mutations.ts
+++ src/features/authorization/api/auth.mutations.ts
@@ -16,6 +16,7 @@ export const useCreateUser = () => {
     mutationFn: (data: CreateUserDto) => authApi.register(data),
     onSuccess: async ({ accessToken, user }) => {
       await queryClient.invalidateQueries({ queryKey: userKeys.me() });
+      queryClient.setQueryData(userKeys.me(), user);
       setToken(accessToken);
       setUser(user);
     },
@@ -31,6 +32,7 @@ export const useLoginUser = () => {
     mutationFn: (data: LoginUserDto) => authApi.login(data),
     onSuccess: async ({ accessToken, user }) => {
       await queryClient.invalidateQueries({ queryKey: userKeys.me() });
+      queryClient.setQueryData(userKeys.me(), user);
       setToken(accessToken);
       setUser(user);
     },
